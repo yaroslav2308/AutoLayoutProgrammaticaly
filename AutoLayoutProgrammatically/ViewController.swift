16218544ad1803f18a478c6d1857f742a9b98da8
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-04-18 23:28:54 +0300

    SafeArea layout

--- AutoLayoutProgrammatically/ViewController.swift
+++ AutoLayoutProgrammatically/ViewController.swift
@@ -41,17 +41,42 @@ class ViewController: UIViewController {
         textView.translatesAutoresizingMaskIntoConstraints = false
         return textView
     }()
+    
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        button.setTitleColor(.gray, for: .normal)
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        button.setTitleColor(.red, for: .normal)
+        return button
+    }()
+    
+    private let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        pc.pageIndicatorTintColor = .gray
+        pc.currentPageIndicatorTintColor = .red
+        pc.translatesAutoresizingMaskIntoConstraints = false
+        return pc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        view.addSubview(redSquareImageView)
         view.addSubview(descriptionTextView)
         
+        setupBottomControls()
+        
         setupLayout()
-        
-        
-        
     }
     
     private func setupLayout() {
@@ -81,6 +106,24 @@ class ViewController: UIViewController {
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
 
         
+    }
+    
+    private func setupBottomControls() {
+        let redView = UIView()
+        redView.backgroundColor = .red
+        
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlsStackView.distribution = .fillEqually
+        
+        view.addSubview(bottomControlsStackView)
+        
+        NSLayoutConstraint.activate([
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 
 
